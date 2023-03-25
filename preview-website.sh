48822d3daa2326120d5288ffb44c6aca1860b2b6
Janaka Abeywardhana <contact@janaka.co.uk>
2023-03-25 15:14:31 +0000

    build: switch local serv to incremental build

--- preview-website.sh
+++ preview-website.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
-
-docker run --rm -it -p 8000:8000 -v $PWD:/docs squidfunk/mkdocs-material
+# the entry point is `mkdocs serve` @see https://www.mkdocs.org/user-guide/cli/#mkdocs-serve
+# `/docs` is the default path for the documentation
+# `--dirtyreload` is used to only rebuild pages that have changed in development mode.
+docker run --rm -it -p 8000:8000 -v $PWD:/docs squidfunk/mkdocs-material serve --dev-addr="0.0.0.0:8000" --no-livereload --dirtyreload
